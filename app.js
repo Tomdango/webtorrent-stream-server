b18dff8d7a975ed3395d85d20d0e41fdb4103628
Thomas Judd-Cooper <me@tomjuddcooper.co.uk>
2019-04-14 22:54:29 +0100

    CORS

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.header(
     'Access-Control-Allow-Headers',
